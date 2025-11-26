a16fe77641eec3820df467c1bd7bc4619fc0642c
André Igor <andreigor.17@hotmail.com>
2025-11-25 21:11:06 -0300

    Update JakartaRestConfiguration.java

--- src/main/java/br/com/irontech/phbchampreloaded/JakartaRestConfiguration.java
+++ src/main/java/br/com/irontech/phbchampreloaded/JakartaRestConfiguration.java
@@ -1,25 +1,27 @@
 package br.com.irontech.phbchampreloaded;
 
-import br.com.irontech.phbchampreloaded.resources.GSIResource;
-import br.com.irontech.phbchampreloaded.resources.JakartaEE10Resource;
-import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
-import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Configures Jakarta RESTful Web Services for the application.
- * @author Juneau
+ *
+ * @author Andre
  */
-@ApplicationPath("resources")
+@jakarta.ws.rs.ApplicationPath("resources")
 public class JakartaRestConfiguration extends Application {
-    
+
     @Override
     public Set<Class<?>> getClasses() {
-        Set<Class<?>> classes = new HashSet<>();
+
         // Registra os recursos JAX-RS
-        classes.add(GSIResource.class);
-        classes.add(JakartaEE10Resource.class);
-        return classes;
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(br.com.irontech.phbchampreloaded.resources.GSIResource.class);
+        resources.add(br.com.irontech.phbchampreloaded.resources.JakartaEE10Resource.class);
     }
 }
